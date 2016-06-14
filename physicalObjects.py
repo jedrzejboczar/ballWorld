babde533e47535b07a5fda4c661bb70f2b7ab778
Yendreij <yendreij@gmail.com>
2016-06-14 16:58:58 +0200

    Init. Pilka dzialajaca na zasadach fizyki newtonowskiej z grawitacja itd. Zmienic zasade dzialania odbic od rogow planszy.

---
+++ physicalObjects.py
@@ -0,0 +1,104 @@
+import math
+import pygame
+
+class PhysicalObject(object):
+	def __init__(self, mass, position, velocity=(0, 0), acceleration=(0, 0), force=(0, 0)):
+		self.m = mass
+		self.x, self.y = position	# (x,y)
+		self.vx, self.vy = velocity
+		self.ax, self.ay = acceleration
+		self.fx, self.fy = force
+
+	def move(self, borders):
+		self.countPhysics()
+		self.x += self.vx
+		self.y -= self.vy	# WAZNE: bo na wyswietlaniu y jest odwrocony
+		self.checkCollisions(borders)
+
+	def checkCollisions(self, borders):
+		pass
+
+	def countPhysics(self):
+		self.ax = self.fx / self.m
+		self.ay = self.fy / self.m
+		self.vx += self.ax
+		self.vy += self.ay
+
+	def draw(self):
+		pass
+
+
+
+class Ball(PhysicalObject):
+	def __init__(self, mass, position, velocity=(0, 0), acceleration=(0, 0), force=(0, 0)):
+		PhysicalObject.__init__(self, mass, position, velocity, acceleration, force)
+		self.radius = 18
+		self.color = (215, 20, 20)
+
+	def draw(self, paintScreen):
+		x, y = int(self.x), int(self.y)
+		pygame.draw.circle(paintScreen, self.color, (x, y), self.radius)
+
+	def checkCollisions(self, borders):
+		if self.x - self.radius < 0: # lewa sciana
+			if self.y - self.radius < 0: # lewy gorny rog
+				print "lewy gorny rog"
+				pass
+			elif self.y + self.radius > borders[1]: # lewy dolny rog
+				print "lewy dolny rog"
+				pass
+			else:	#lewa sciana
+				print "lewa sciana"
+				self.vx = abs(self.vx)
+				self.x = 0 + self.radius
+				# self.ax = abs(self.ax)
+				pass
+		elif self.x + self.radius > borders[0]: # prawa sciana
+			if self.y - self.radius < 0: # prawy gorny rog
+				print "prawy gorny rog"
+				pass
+			elif self.y + self.radius > borders[1]: # prawy dolny rog
+				print "prawy dolny rog"
+				pass
+			else: # prawa sciana
+				print "prawa sciana"
+				self.vx = -abs(self.vx)
+				self.x = borders[0] - self.radius
+				# self.ax = -abs(self.ax)
+				pass
+		else: # zostaje przypadek gornej i dolnej sciany
+			if self.y - self.radius < 0: # gorna sciana
+				print "gorna sciana"
+				self.vy = -abs(self.vy)
+				self.y = 0 + self.radius
+				# self.ay = -abs(self.ay)
+				pass
+			elif self.y + self.radius > borders[1]: # dolna sciana
+				print "dolna sciana"
+				self.vy = abs(self.vy)
+				self.y = borders[1] - self.radius
+				# self.ay = abs(self.ay)
+				pass
+
+
+
+def distance((x1,y1), (x2, y2)):
+	d = math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+	return d
+
+		# v = cos(dir) * vx
+		# cos(dir) = v/vx
+		# vx = v / cos(dir)
+
+if __name__ == '__main__':	# CO Z TA MASA??? + nie dziala dla dir=0 - dziel/0
+	o = PhysicalObject(10, (2,2), (0.1, 0.3))
+	print o.x, o.y
+	o.move(1)
+	print o.x, o.y
+	o.move(3)
+	print o.x, o.y
+	o.move(10)
+	o.v = -0.3
+	print o.x, o.y
+	o.move(5)
+	print o.x, o.y
---
+++ world.py
@@ -0,0 +1,51 @@
+from pygamehelper import PygameHelper
+from physicalObjects import *
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+class World(PygameHelper):
+	def __init__(self, objects=[]):
+		PygameHelper.__init__(self, (640, 480), WHITE)
+		# super(self.__class__, self).__init__()
+		self.objects = objects
+
+		self.y_max = 640
+
+	def mainLoop(self):
+		PygameHelper.mainLoop(self, 40)
+		# super(self.__class__, self).mainLoop(40)
+
+
+	def doPhysics(self):
+		for obj in self.objects:
+			pass
+
+	def update(self):
+		for o in self.objects:
+			o.move(self.size)
+
+	def draw(self):
+		self.screen.fill(WHITE)
+		for o in self.objects:
+			o.draw(self.screen)
+		o1 = self.objects[0]
+		if o1.y < self.y_max:
+			self.y_max = o1.y
+		text = pygame.font.SysFont("serif", 15).render("y_max = (" + str(640 - self.y_max) + ")", True, BLACK)
+		self.screen.blit(text, (20, 345))
+		text = pygame.font.SysFont("serif", 15).render("x = (" + str(o1.x) + ", " + str(o1.y) + ")", True, BLACK)
+		self.screen.blit(text, (20, 370))
+		text = pygame.font.SysFont("serif", 15).render("v = (" + str(o1.vx) + ", " + str(o1.vy) + ")", True, BLACK)
+		self.screen.blit(text, (20, 395))
+		text = pygame.font.SysFont("serif", 15).render("a = (" + str(o1.ax) + ", " + str(o1.ay) + ")", True, BLACK)
+		self.screen.blit(text, (20, 420))
+		text = pygame.font.SysFont("serif", 15).render("F = (" + str(o1.fx) + ", " + str(o1.fy) + ")", True, BLACK)
+		self.screen.blit(text, (20, 445))
+
+
+
+if __name__ == '__main__':
+	o = Ball(mass=20, position=(140,180), velocity=(4.5, 0), force=(0, -10))
+	world = World([o])
+	world.mainLoop()
